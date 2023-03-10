424e22de16ade4c14763c3b2679d0066f31b84ba
JasonBright <jbright.ru@gmail.com>
2023-03-10 21:43:12 +0200

    dodik blyat

--- Assets/yoba/Runtime/JaYobaScript.cs
+++ Assets/yoba/Runtime/JaYobaScript.cs
@@ -7,7 +7,7 @@ public class JaYobaScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log( $"blyat     " );   
+        Debug.Log( $"blyat     loh" );   
     }
 
     // Update is called once per frame
